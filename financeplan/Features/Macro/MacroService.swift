394cf3613b726b357994d5114137b1e745440e37
FACorreiaa <fernandocorreia316@gmail.com>
2026-07-09 14:22:10 +0100

    add more things

--- financeplan/Features/Macro/MacroService.swift
+++ financeplan/Features/Macro/MacroService.swift
@@ -1,33 +1,33 @@
 import Foundation
 import StockPlanShared
 
-public protocol MacroServicing: Sendable {
+protocol MacroServicing: Sendable {
   func getCurrentInflation(country: String?) async throws -> InflationSnapshotResponse
   func getTopMovers(country: String?, focus: String?) async throws -> [TopMoverDTO]
   func getSeries(country: String?, series: String) async throws -> MacroSeriesResponse
   func getSupportedCountries() async throws -> [SupportedCountry]
 }
 
-public final class MacroHTTPService: MacroServicing {
+final class MacroHTTPService: MacroServicing {
   private let httpClient: MacroHTTPClient
 
-  public init(httpClient: MacroHTTPClient) {
+  init(httpClient: MacroHTTPClient) {
     self.httpClient = httpClient
   }
 
-  public func getCurrentInflation(country: String?) async throws -> InflationSnapshotResponse {
+  func getCurrentInflation(country: String?) async throws -> InflationSnapshotResponse {
     try await httpClient.getCurrentInflation(country: country)
   }
 
-  public func getTopMovers(country: String?, focus: String?) async throws -> [TopMoverDTO] {
+  func getTopMovers(country: String?, focus: String?) async throws -> [TopMoverDTO] {
     try await httpClient.getTopMovers(country: country, focus: focus)
   }
 
-  public func getSeries(country: String?, series: String) async throws -> MacroSeriesResponse {
+  func getSeries(country: String?, series: String) async throws -> MacroSeriesResponse {
     try await httpClient.getSeries(country: country, series: series)
   }
 
-  public func getSupportedCountries() async throws -> [SupportedCountry] {
+  func getSupportedCountries() async throws -> [SupportedCountry] {
     try await httpClient.getSupportedCountries()
   }
 }
